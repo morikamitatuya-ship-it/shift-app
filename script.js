830206e1071ca09f9de1d209463e9b3832956a8f
morikamitatuya-ship-it <morikamitatuya@gmail.com>
2026-07-04 18:33:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,84 @@
+// ===============================
+// GR TRACK MANAGER
+// script.js
+// ===============================
 
+let currentShift = 1;
+
+// ボタン取得
+const shift1Button = document.getElementById("shift1");
+const shift2Button = document.getElementById("shift2");
+
+// ----------------------
+// 勤務切替
+// ----------------------
+
+shift1Button.addEventListener("click", () => {
+
+    currentShift = 1;
+
+    shift1Button.classList.add("active");
+    shift2Button.classList.remove("active");
+
+    console.log("1直");
+
+});
+
+shift2Button.addEventListener("click", () => {
+
+    currentShift = 2;
+
+    shift2Button.classList.add("active");
+    shift1Button.classList.remove("active");
+
+    console.log("2直");
+
+});
+
+// ----------------------
+// メンバー管理
+// ----------------------
+
+document
+.getElementById("memberButton")
+.addEventListener("click",()=>{
+
+    alert("メンバー管理画面（これから作ります😊）");
+
+});
+
+// ----------------------
+// スキル管理
+// ----------------------
+
+document
+.getElementById("skillButton")
+.addEventListener("click",()=>{
+
+    alert("スキル管理画面（これから作ります😊）");
+
+});
+
+// ----------------------
+// 早出表作成
+// ----------------------
+
+document
+.getElementById("createButton")
+.addEventListener("click",()=>{
+
+    alert("早出表を作成します");
+
+});
+
+// ----------------------
+// 再抽選
+// ----------------------
+
+document
+.getElementById("rerollButton")
+.addEventListener("click",()=>{
+
+    alert("再抽選します");
+
+});
