7783debbd1512eb800051e9d99b702b5f2762f7f
morikamitatuya-ship-it <morikamitatuya@gmail.com>
2026-07-13 20:28:40 +0900

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -1,1 +1,17 @@
+// ===============================
+// GR TRACK MANAGER
+// Scheduler Engine
+// ===============================
 
+function createSchedule() {
+
+    const whiteboard = document.getElementById("whiteboard");
+
+    const days = ["月", "火", "水", "木", "金"];
+
+    whiteboard.innerHTML = `
+        <h2>Scheduler OK!</h2>
+        <p>エンジンから表示しています。</p>
+    `;
+
+}
